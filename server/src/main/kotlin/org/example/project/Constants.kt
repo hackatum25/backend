101d55e2cc3b8d15779bbf857c625e9e1a36700f
c3ntur1o <44846221+c3ntur1o@users.noreply.github.com>
2025-11-22 03:08:53 +0100

    posts table

--- server/src/main/kotlin/org/example/project/Constants.kt
+++ server/src/main/kotlin/org/example/project/Constants.kt
@@ -2,4 +2,5 @@ package org.example.project
 
 const val SERVER_PORT = 8080
 
-const val MAX_DB_VARCHAR_LENGTH = 50
+const val MAX_TITLE_LENGTH = 100
+const val MAX_DESCRIPTION_LENGTH = 10000
--- server/src/main/kotlin/org/example/project/Posts.kt
+++ server/src/main/kotlin/org/example/project/Posts.kt
@@ -1,10 +1,8 @@
 package org.example.project
 
-import org.jetbrains.exposed.v1.core.Table
+import org.jetbrains.exposed.v1.core.dao.id.IntIdTable
 
-object PostsTable : Table() {
-    val id = integer("id").autoIncrement()
-    val sequelId = integer("sequel_id").uniqueIndex()
-    val name = varchar("name", MAX_DB_VARCHAR_LENGTH)
-    val director = varchar("director", MAX_DB_VARCHAR_LENGTH)
+object Posts : IntIdTable() {
+    val title = varchar("title", MAX_TITLE_LENGTH)
+    val description = varchar("description", MAX_DESCRIPTION_LENGTH)
 }
