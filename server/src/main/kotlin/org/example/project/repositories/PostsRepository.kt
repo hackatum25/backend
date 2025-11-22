8f6f8846d608732b8935057a77e1f51f98b6cccd
c3ntur1o <44846221+c3ntur1o@users.noreply.github.com>
2025-11-22 11:38:46 +0100

    endpoint for posting posts

--- server/src/main/kotlin/org/example/project/repositories/PostsRepository.kt
+++ server/src/main/kotlin/org/example/project/repositories/PostsRepository.kt
@@ -3,11 +3,21 @@ package org.example.project.repositories
 import org.example.project.db.PostDAO
 import org.example.project.db.daoToModel
 import org.example.project.model.Post
+import org.jetbrains.exposed.v1.jdbc.insert
 import org.jetbrains.exposed.v1.jdbc.transactions.suspendTransaction
+import org.example.project.db.PostTable
 
 class PostsRepository {
     suspend fun getPost(postId: Int): Post = suspendTransaction {
         daoToModel(PostDAO[postId])
+    }
+
+    suspend fun newPost(post: Post) : Int = suspendTransaction {
+        val mypost = PostTable.insert{
+            it[PostTable.title] = post.title
+            it[PostTable.description] = post.description
+        }
+        return@suspendTransaction 1
     }
 
     private suspend fun upvotings(postId: Int) {
--- server/src/main/kotlin/org/example/project/routes/PostRoutes.kt
+++ server/src/main/kotlin/org/example/project/routes/PostRoutes.kt
@@ -1,8 +1,11 @@
 package org.example.project.routes
 
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
+import io.ktor.server.routing.post
 import io.ktor.server.util.getValue
 import org.example.project.model.Post
 import org.example.project.repositories.PostsRepository
@@ -14,4 +17,10 @@ fun Route.postRoutes() {
         val post: Post = postsRepository.getPost(id)
         call.respond(post)
     }
+    post("/posts"){
+        val post = call.receive<Post>()
+        postsRepository.newPost(post)
+        call.respond(status = HttpStatusCode.Created, message = post)
+    }
+
 }
