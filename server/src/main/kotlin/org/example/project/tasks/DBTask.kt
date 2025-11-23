38cca28bde78e26d588fc0d3f1522347162250ed
Fabi987 <Fabian_L03@gmx.net>
2025-11-23 09:41:37 +0100

    adjust seeds script

--- server/src/main/kotlin/org/example/project/tasks/DBTask.kt
+++ server/src/main/kotlin/org/example/project/tasks/DBTask.kt
@@ -72,6 +72,13 @@ fun main() {
             createdBy = munich.id
         }
         val post2 = PostDAO.new {
+            title = "Closure of the Eisbachwelle"
+            description =
+                "The Eisbachwelle is one of Munich’s most iconic spots, drawing surfers and spectators from around the world. However, the wave slowly decreases in power. Thus, the question arises: should we intervene? Supporters argue that maintaining the wave preserves a unique cultural landmark and keeps the local surf community alive. Critics caution that human interference may disrupt the natural flow of the river. Ultimately, the decision comes down to whether preserving this urban surf tradition justifies carefully managed adjustments to the Eisbach."
+            createdAt = LocalDateTime.now().minusDays(8).toKotlinLocalDateTime()
+            createdBy = user2.id
+        }
+        val post3 = PostDAO.new {
             title = "Fully autonomous subways"
             description =
                 "The idea of fully autonomous subways raises both excitement and concern. On one hand, driverless trains could improve efficiency, reduce delays, and operate more safely through advanced sensors and automation. On the other hand, many worry about technical failures, cybersecurity risks, and the loss of human presence during emergencies. Whether subways should run without a human ultimately depends on balancing innovation with safety, trust, and the needs of passengers." +
@@ -79,42 +86,21 @@ fun main() {
             createdAt = LocalDateTime.now().minusDays(1).toKotlinLocalDateTime()
             createdBy = munich.id
         }
-        val post3 = PostDAO.new {
+        val post4 = PostDAO.new {
             title = "District commitee of Schwabing"
             description =
-                "This Friday, there will be the next meeting of the district committee of Schwabing. One important point of the meeting will be the increasing shortage of living space in our district and on how we as the district committee can tackle this problem;" +
+                "This Friday, there will be the next meeting of the district committee of Schwabing. One important point of the meeting will be the inadequate bike infrastructure and how we as the district committee can tackle this problem;" +
                         "you can find the whole agenda on our website. Please note that the meeting is open to everyone, we are always happy about attendees, independent on whether you just want to actively listen or even contribute to finding solutions to the issues in our district." +
                         "If there are further topics which you want to discuss in the next meeting, feel free to write as an email or contact us in some other way (for detailed information, please see our website)."
             createdAt = LocalDateTime.now().minusDays(3).toKotlinLocalDateTime()
             createdBy = user1.id
         }
-        val post4 = PostDAO.new {
-            title = "Cras eleifend turpis fames primis vulputate ornare sagittis"
+        val post5 = PostDAO.new {
+            title = "U6 extension to Martinsried"
             description =
-                "Sem placerat in id cursus mi pretium tellus. Orci varius natoque penatibus et magnis dis parturient. Finibus facilisis dapibus etiam interdum tortor ligula congue. Proin libero feugiat tristique accumsan maecenas potenti ultricies. Sed diam urna tempor pulvinar vivamus fringilla lacus. Eros lobortis nulla molestie mattis scelerisque maximus eget. Porta elementum a enim euismod quam justo lectus. Curabitur facilisi cubilia curae hac habitasse platea dictumst. Nisl malesuada lacinia integer nunc posuere ut hendrerit. Efficitur laoreet mauris pharetra vestibulum fusce dictum risus. "
+                "The planned U6 extension to Martinsried aims to better connect the research campus, residential areas, and local businesses to Munich’s subway network. By adding this link, commuters would gain a faster, more reliable alternative to buses and cars, easing traffic congestion and strengthening the region’s role as a hub for science and innovation. The project promises improved mobility and new opportunities for both residents and visitors. According to the current plan the project will be completed in 2027."
             createdAt = LocalDateTime.now().minusDays(7).toKotlinLocalDateTime()
             createdBy = user1.id
-        }
-        val post5 = PostDAO.new {
-            title = "Imperdiet mollis nullam volutpat porttitor ullamcorper rutrum gravida"
-            description =
-                "Ut dipiscing elit quisque faucibus ex sapien vitae pellentesque. Ad litora torquent per conubia nostra inceptos himenaeos. Consequat magna ante condimentum neque at luctus nibh. Ornare sagittis vehicula praesent dui felis venenatis ultrices. Pretium tellus duis convallis tempus leo eu aenean. Dis parturient montes nascetur ridiculus mus donec rhoncus. Ligula congue sollicitudin erat viverra ac tincidunt nam. Potenti ultricies habitant morbi senectus netus suscipit auctor."
-            createdAt = LocalDateTime.now().minusDays(8).toKotlinLocalDateTime()
-            createdBy = user2.id
-        }
-        val post6 = PostDAO.new {
-            title = "Pringilla lacus nec metus bibendum egestas iaculis massa"
-            description =
-                "Maximus eget fermentum odio phasellus non purus est. Justo lectus commodo augue arcu dignissim velit aliquam. Platea dictumst lorem ipsum dolor sit amet consectetur. Ut hendrerit semper vel class aptent taciti sociosqu. Dictum risus blandit quis suspendisse aliquet nisi sodales. Rutrum gravida cras eleifend turpis fames primis vulputate. Vitae pellentesque sem placerat in id cursus mi. Inceptos himenaeos orci varius natoque penatibus et magnis. Luctus nibh finibus facilisis dapibus etiam interdum tortor. "
-            createdAt = LocalDateTime.now().minusDays(15).toKotlinLocalDateTime()
-            createdBy = user2.id
-        }
-        val post7 = PostDAO.new {
-            title = "Venenatis ultrices proin libero feugiat tristique accumsan maecenas"
-            description =
-                "Eu aenean sed diam urna tempor pulvinar vivamus. Donec rhoncus eros lobortis nulla molestie mattis scelerisque. Tincidunt nam porta elementum a enim euismod quam. Suscipit auctor curabitur facilisi cubilia curae hac habitasse. Iaculis massa nisl malesuada lacinia integer nunc posuere. Purus est efficitur laoreet mauris pharetra vestibulum fusce. Velit aliquam imperdiet mollis nullam volutpat porttitor ullamcorper. Amet consectetur adipiscing elit quisque faucibus ex sapien. Taciti sociosqu ad litora torquent per conubia nostra. Nisi sodales consequat magna ante condimentum neque at. Primis vulputate ornare sagittis vehicula praesent dui felis. Cursus mi pretium tellus duis convallis tempus leo."
-            createdAt = LocalDateTime.now().minusDays(20).toKotlinLocalDateTime()
-            createdBy = bavaria.id
         }
 
         val rating1 = RatingDAO.new {
@@ -196,16 +182,6 @@ fun main() {
             user = user1.id
             post = post5.id
             rating = 1
-        }
-        val rating17 = RatingDAO.new {
-            user = user2.id
-            post = post6.id
-            rating = 1
-        }
-        val rating18 = RatingDAO.new {
-            user = user3.id
-            post = post6.id
-            rating = -1
         }
         val rating19 = RatingDAO.new {
             user = user4.id
