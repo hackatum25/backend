8eabfdd66875b936944aa2e09724db1ab0c5c34c
c3ntur1o <44846221+c3ntur1o@users.noreply.github.com>
2025-11-23 03:29:55 +0100

    add profile endpoint

--- server/src/main/kotlin/org/example/project/repositories/UserRepository.kt
+++ server/src/main/kotlin/org/example/project/repositories/UserRepository.kt
@@ -3,6 +3,7 @@ package org.example.project.repositories
 import org.jetbrains.exposed.v1.jdbc.transactions.suspendTransaction
 import org.example.project.db.UserDAO
 import org.example.project.db.findIdByUsername
+import org.example.project.model.Profile
 
 class UserRepository {
     suspend fun getUser(username: String) : Int? =
@@ -10,5 +11,14 @@ class UserRepository {
             findIdByUsername(username)
         }
 
+    suspend fun getProfile(userID: Int) : Profile? =
+        suspendTransaction {
+            val user = UserDAO.findById(userID)
+            if(user == null){
+                null
+            } else {
+                Profile(user.email, user.firstName, user.lastName)
+            }
+        }
 
 }
--- server/src/main/kotlin/org/example/project/routes/UserRoutes.kt
+++ server/src/main/kotlin/org/example/project/routes/UserRoutes.kt
@@ -6,12 +6,14 @@ import io.ktor.server.request.receiveParameters
 import io.ktor.server.response.respond
 import io.ktor.server.response.respondRedirect
 import io.ktor.server.routing.Route
+import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import org.example.project.repositories.UserRepository
 import io.ktor.server.sessions.*
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonObject
 import org.example.project.UserSession
+import org.example.project.model.Profile
 import org.example.project.model.User
 
 
@@ -30,6 +32,21 @@ fun Route.userRoutes() {
                 println("User not found")
             }
         }
+
+    get("/user"){
+        requireLogin(call)?.let{
+            val userID = call.sessions.get<UserSession>()?.userId
+            if(userID == null){
+                call.respond(HttpStatusCode.BadRequest, "Not logged in")
+            } else {
+                val profile: Profile? = userRepository.getProfile(userID)
+                if(profile == null) {
+                    call.respond(HttpStatusCode.BadRequest, "No profile found")
+                } else {
+                call.respond(profile)}
+            }
+        }
+    }
 
 
 }
